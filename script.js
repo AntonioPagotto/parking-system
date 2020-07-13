d483abf8342702ebec8bdac7e4f4cd333bdaae87
AntonioPagotto <antoniopagotto121@gmail.com>
2020-07-13 15:34:48 -0300

    tarde 13 07

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function setaImagem(){
     var settings = {
         primeiraImg: function(){
@@ -72,3 +73,22 @@ function setaImagem(){
 }
 
 window.addEventListener("load",setaImagem,false);
+*/
+
+/*slider*/
+    var mudou = false;
+function mudar(){
+    var bg = document.querySelector('.sld-item')
+
+    if(mudou == false){   
+
+    bg.style.backgroundImage = 'url("img/b2-sld.png")'
+    mudou = true;
+
+    } else {
+
+        bg.style.backgroundImage = 'url("img/b1-sld.png")'
+        mudou = false;
+
+    }
+}
